f1b9386dd88e17d241429b710bed9fae8be4cc68
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2023-06-15 02:23:17 +0000

    ativremota

---
+++ classesbanco/classes/conexao.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def conectar():
+    mydb = mysql.connector.connect(
+        host='database-1.cpdhir0s1jfs.us-east-1.rds.amazonaws.com',
+        user='admin',
+        password='brasil123',
+        database='livraria'
+        )
+    return mydb
